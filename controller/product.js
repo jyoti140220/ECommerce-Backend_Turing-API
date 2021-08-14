d77f5245cde3c38f5b7e326b2ab9302ee8de7d1a
jyoti140220 <jyotibhandari20@navgurukul.org>
2021-08-14 19:06:03 +0530

    modify

--- controller/product.js
+++ controller/product.js
@@ -45,6 +45,7 @@ exports.getProductByCategoryId=async(req,res)=>{
         return res.status(400).json({message: err,status: 404})})
 }
 
+
 exports.getProductByDepartmentId=async(req,res)=>{
     await knex.distinct().from('product').select('product.product_id','product.name','product.description','product.price','product.discounted_price','product.thumbnail')
     .join('product_category','product_category.product_id','=','product.product_id')
